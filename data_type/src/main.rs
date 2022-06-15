da890e6c8f1b4a4f5edda5a18c19c7808d2b5daf
yjj <240304006@163.com>
2022-06-15 09:34:47 +0800

    更新note和数据类型例子

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // error[E0282]: type annotations needed
+    // 在编译器推导中，42解析成的数字有u32，i32等多个数据类型可以标注
+    // 所以必须手动指定该变量的类型
+    // let guess = "42".parse().expect("Not a number");
+    let guess: u32 = "42".parse().expect("Not a number");
+    println!("guess value: {}", guess);
 }
