3ce52eb6936752e3ea7f372c6d06da2bf7256baf
yjj <240304006@163.com>
2022-06-15 08:59:01 +0800

    添加数据类型例子和.gitignore

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,23 @@
-const MAX_POINTS: i32 = 100_000;
+// const MAX_POINTS: u32 = 100_000;
+
+// fn main() {
+//     const MAX_POINTS: u32 = 100_000;
+//     // let x = 5;  // 不能对不可变的变量二次赋值
+//     let mut x = 5;  // 将该变量声明成可变的
+//     println!("The value of x is {}", x);
+//     x = 6;
+//     println!("The value of x is {}", x);
+// }
+
+// fn main() {
+//     let x = 5;
+//     // x = x + 1;  // 此处会有问题，因为 x 已经是一个不可变的变量，后续进行的是赋值操作
+//     let x = x + 1;  // 此处没问题，因为新声明的变量 x 覆盖（隐藏）了旧的变量 x，从而可以编译通过
+//     println!("The value of x is {}", x);
+// }
 
 fn main() {
-    const MAX_POINTS: i32 = 100_000;
-
-    // let x = 5;  // 不能对不可变的变量二次赋值
-    let mut x = 5;  // 将该变量声明成可变的
-    print!("The value of x is {}", x);
-
-    x = 6; 
-    print!("The value of x is {}", x);
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("{}", spaces);
 }
